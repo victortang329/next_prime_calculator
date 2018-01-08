fc668cacf5fd871c605517638f0493721a922cf2
victortang329 <victortang329@gmail.com>
2018-01-07 18:03:43 -0800

    added function checking if number is prime

--- prime_calculator.c
+++ prime_calculator.c
@@ -6,6 +6,8 @@
 
 int next_prime (int number);
 
+int prime_detect (int number);
+
 /*
 int main (int argc, char* argv[]) {
 	if (isdigit(argv[1])){;
@@ -19,9 +21,10 @@ int main (int argc, char* argv[]) {
 
 // for testing 
 int main (void) {
-	int number = 0;
-	while (number < 99){
-		int next_prime_number = next_prime(number);
+	int number = 3;
+	while (number < 99999){
+//		int next_prime_number = next_prime(number);
+		int next_prime_number = prime_detect(number);
 		number++;
 	}
 
@@ -43,6 +46,28 @@ int next_prime (int number){
 			return test_n;
 		}
 		// if not divisible, try reduce denominator by 1
+		else {
+			test_d--;
+		}
+	}
+}
+
+int prime_detect (int number) {
+	int test_n = number;
+	int test_d = number-1;
+
+	while (number > -1) {
+		if (test_n%test_d == 0 && test_d > 1){
+			test_n++;
+			test_d = test_n-1;
+		}
+		else if (test_n%test_d == 0 && test_d == 1 && test_n == number) {
+			printf("%d\n", test_n);
+			return test_n;
+		}
+		else if (test_n%test_d == 0 && test_d == 1 && test_n != number) {
+			return test_n;
+		}
 		else {
 			test_d--;
 		}
